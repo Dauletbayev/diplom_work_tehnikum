95c28d394a39e3047d5d5ade8c5b9ee54bdf1ee1
Dauletbayev <dauletbayev2405@gmail.com>
2024-05-13 20:06:31 +0500

    reg user updated

--- api/users_api/users.py
+++ api/users_api/users.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request
 from pydantic import BaseModel
 from database.userservice import *
+import re
 
 class UserRegistration(BaseModel):
     first_name: str
@@ -12,15 +13,25 @@ class UserRegistration(BaseModel):
     country: str
     city: str
 
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Za-z]{2,})+')
+
+def mail_checker(email):
+    if re.fullmatch(regex, email):
+        return True
+    return False
+
 users_router = APIRouter(tags=['Управление пользователями'], prefix='/users')
 
 @users_router.post('/api/register')
 async def register_user(user_data: UserRegistration):
-    user_id = register_user_db(**user_data.dict())
-    if isinstance(user_id, int):
-        return {"message": "User registered successfully", "user_id": user_id}
-    else:
-        return {"message": "Registration failed", "reason": user_id}
+    mail_validation = mail_checker(user_data.email)
+    if mail_validation:
+        try:
+            reg_user = register_user_db(**user_data.dict())
+            return {'status': 1, 'message': reg_user}
+        except Exception as e:
+            return {'status': 0, 'message': str(e)}
+    return {'status': 0, 'message': 'Invalid email or phone'}
 
 @users_router.post('/api/user')
 async def get_user(user_id: int):
